c5cc7e228f04945ff23e57110c5f0119454197e0
Nevena <Djaja@Nevenas-MacBook-Air.local>
2014-02-22 13:42:24 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rawfusion::Application.config.secret_key_base = 'a49d136db8f68dee5a312eeb103796cd5d3fe3d1ee054fc2b807860198292fb6b6ee41ba2ab3b0f27e628ca56577dd670668be1b0438a275eaac9b5b47b3c893'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rawfusion::Application.config.session_store :cookie_store, key: '_rawfusion_session'
